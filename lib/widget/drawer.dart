7cdcb72363dad2c1f2be0b7cf2e1c979fbc96fa6
yogeshkumar8755 <yogeshshar179@gmail.com>
2023-07-03 18:04:31 +0530

    drawer

--- lib/widget/drawer.dart
+++ lib/widget/drawer.dart
@@ -1,13 +1,12 @@
 // ignore_for_file: prefer_const_constructors
 
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 class MyDrawer extends StatelessWidget {
   const MyDrawer({super.key});
 
-final ImageURL ="https://media.istockphoto.com/id/470100848/vector/male-profile-icon-white-on-the-blue-background.jpg?s=1024x1024&w=is&k=20&c=sawh7MhVFsl__2Bb5wAE317Pss1Vkd8843fbBcihkWA=";
+  final ImageURL =
+      "https://media.istockphoto.com/id/470100848/vector/male-profile-icon-white-on-the-blue-background.jpg?s=1024x1024&w=is&k=20&c=sawh7MhVFsl__2Bb5wAE317Pss1Vkd8843fbBcihkWA=";
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -16,35 +15,40 @@ final ImageURL ="https://media.istockphoto.com/id/470100848/vector/male-profile-
         child: ListView(
           // ignore: prefer_const_literals_to_create_immutables
           children: [
-            // ignore: prefer_const_constructors
             DrawerHeader(
-              
               padding: EdgeInsets.all(0),
               child: UserAccountsDrawerHeader(
-                margin: EdgeInsets.zero,
-              accountName:Text("yogesh kumar") ,
-               accountEmail: Text("yogeshshar179@gmail.com"),
-               currentAccountPicture: CircleAvatar(backgroundImage: NetworkImage("$ImageURL"),)
-               ),),
-
-               ListTile(
-                 leading: Icon(Icons.home),
-                iconColor: Colors.white,
-                title: Text("Home",
-                style: TextStyle(color: Colors.white),),
-               ),
-                ListTile(
-                leading: Icon(Icons.mail),
-                iconColor: Colors.white,
-                title: Text("Mail",
-                style: TextStyle(color: Colors.white),),
-               ),
-                ListTile(
-                leading: Icon(Icons.settings),
-                iconColor: Colors.white,
-                title: Text("Setting",
-                style: TextStyle(color: Colors.white),),
-               )
+                  margin: EdgeInsets.zero,
+                  accountName: Text("yogesh"),
+                  accountEmail: Text("yogeshshar179@gmail.com"),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundImage: NetworkImage("$ImageURL"),
+                  )),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              iconColor: Colors.white,
+              title: Text(
+                "Home",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.mail),
+              iconColor: Colors.white,
+              title: Text(
+                "Mail",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              iconColor: Colors.white,
+              title: Text(
+                "Setting",
+                style: TextStyle(color: Colors.white),
+              ),
+            )
           ],
         ),
       ),
